5695e3a809d6a34fd01a8d10c98121ca43833f28
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-22 15:38:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,10 @@ def key_for_min_value(name_hash)
 low_key = nil 
 low_val = nil 
 name_hash.each do |key, value|
-  if low_val = nil || 
-
+  if low_val = nil || value < low_val
+    key = low_key
+    value = low_val
 end
+end
+low_key
 end
